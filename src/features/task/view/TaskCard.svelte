2b76740b437671365c062db1e1690eb5baa1e6a9
Gabriel Beal <gabebeal725@gmail.com>
2023-07-16 10:47:42 -0700

    focus mode

--- src/features/task/view/TaskCard.svelte
+++ src/features/task/view/TaskCard.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { fly, slide } from "svelte/transition";
+    import { fade, fly, slide } from "svelte/transition";
     import Utils from "../../../utils/utils";
     import LoadingIndicator from "../../shared/components/LoadingIndicator.svelte";
     import Modal from "../../shared/components/Modal.svelte";
@@ -9,10 +9,17 @@
     export let isSubTask: boolean = false;
     export let depth: number = 0;
 
-    let open = false;
-    let loading = false;
-    let showModal = false;
-    let taskContext = '';
+    let subtasksOpen = false;
+    let focusMode = false;
+
+    function handleClick() {
+        // If subtask enter focus mode, if top level expand subtasks
+        if (isSubTask) {
+            focusMode = true;
+        } else {
+            subtasksOpen = !subtasksOpen;
+        }
+    }
 
 </script>
 
@@ -20,7 +27,9 @@
     {#if isSubTask}
         <div class="h-3 w-1 mr-5 ml-auto z-0 depth-{depth % 6}"/>
     {/if}
-    <div class="flex flex-row px-4 py-3 bg-secondary rounded-md drop-shadow text-white text-md w-full sm:w-3/4 md:w-1/2 mx-auto border-2 border-opacity-75 border-secondary z-10 {isSubTask ? '': 'mt-3'} relative" class:opacity-20={$task.isComplete}>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div class="flex flex-row px-4 py-3 bg-secondary rounded-md drop-shadow text-white text-md w-full sm:w-3/4 md:w-1/2 mx-auto border-2 border-opacity-75 border-secondary z-10 {isSubTask ? '': 'mt-3'} relative" class:opacity-20={$task.isComplete} on:click={handleClick}>
         <button class:bg-slate-800={!$task.isComplete} class:hover:bg-slate-700={!$task.isComplete} class:bg-ice={$task.isComplete} class="rounded-full border-2 border-ice border-opacity-75 hover:cursor-pointer h-6 w-6 min-h-[1.5rem] min-w-[1.5rem] max-h-6 max-w-6 my-auto" on:click={() => task.markComplete(!$task.isComplete)}/>
         <div class="flex flex-col ml-3" class:line-through={$task.isComplete}>
             <p class="font-medium">{task.state.title}</p>
@@ -30,15 +39,14 @@
         </div>
         {#if $task.subTasks.length > 0}
             <button
-                class:rotate-90={!open}
-                on:click={() => open = !open}
+                class:rotate-90={!subtasksOpen}
                 class="material-icons ease-out duration-300 arrow-icon opacity-75 cursor-pointer select-none">
                 arrow_drop_down
             </button>
         {/if}
         
-    </div>
-    {#if open}
+        </div>
+    {#if subtasksOpen}
         <div transition:slide={{duration: 250}}>
             {#each task.state.subTasks as subTask}
                 <svelte:self task={subTask} isSubTask={true} depth={depth + 1}/>
@@ -48,6 +56,20 @@
     {/if}
 </div>
 
+<!-- Enter Focus Mode -->
+<!-- svelte-ignore missing-declaration -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+    transition:fade={{duration: 250}}
+    class="fixed inset-0 z-40 bg-black bg-opacity-50 backdrop-blur px-10"
+    class:hidden={!focusMode}
+    on:click={() => focusMode = false}
+    >
+    <h1 class="text-white text-5xl font-medium pt-10 text-center mt-36">Focus Mode</h1>
+    <h2 class="mx-auto mt-10 text-2xl">Task</h2>
+    <h2 class="mx-auto text-2xl">{task.state.title}</h2>
+</div>
 
 <style>
     button.arrow-icon {
